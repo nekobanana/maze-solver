8a48d21b17db357e570f25e0e7bc33148d65b781
nekobanana <pru98@hotmail.it>
2023-11-13 23:11:35 +0100

    Loops for mazes and runs

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@
 #include <filesystem>
 #include <omp.h>
 
+#ifdef _OPENMP
 #define N_PARTICLES 100
-
-std::string pathToString(std::vector<std::pair<int, int>> &path);
+#define getThreadNumber() omp_get_thread_num()
+#else
+#define N_PARTICLES 1
+#define getThreadNumber() (-1)
+#endif
+#define N_MAZES 7
+#define N_RUNS 100
+#define SAVE_SOLUTION_IMG false
 
 int main() {
     #ifdef _OPENMP
@@ -14,45 +21,59 @@ int main() {
     #endif
     cv::RNG rng( 0 );
     std::filesystem::path basePath = std::filesystem::current_path();
-    std::filesystem::path imgPath = std::filesystem::relative("images/5.png");
-    Maze maze;
-    maze.load_maze_from_image((basePath / imgPath).string());
-    if (!maze.isStartSet())
-        return -1;
-    Cell startCell = maze.getStartCell();
-    bool solution_found = false;
-    #pragma omp parallel for default(none), firstprivate(startCell, rng), shared(solution_found, maze, std::cout)
-    for (int i = 0; i < N_PARTICLES; i++) {
-        Cell cell = startCell;
-        std::vector<std::pair<int, int>> path;
-        bool out = false;
-        while (!out && !solution_found) {
-            path.push_back(std::pair(cell.x, cell.y));
-            int random = rng.uniform(0, cell.numPossibleDirections);
-            try {
-                cell = maze.move(cell, cell.getDirectionFromIndex(random));
-            }
-            catch (OutOfMazeException &e) {
-                out = true;
-                solution_found = true;
+    double mazes_total_sum_run_time = 0;
+    for (int maze_index = 0; maze_index < N_MAZES; maze_index++) {
+        std::filesystem::path imgPath = std::filesystem::relative("images/" + std::to_string(maze_index) + ".png");
+        Maze maze;
+        maze.load_maze_from_image((basePath / imgPath).string());
+        if (!maze.isStartSet())
+            return -1;
+        Cell startCell = maze.getStartCell();
+        double maze_sum_run_time = 0;
+        std::cout <<std::endl << "Maze " << maze_index << std::endl;
+        for (int run = 0; run < N_RUNS; run++) {
+            bool solution_found = false;
+            int solution_thread_n = -1;
+//            double startTime = omp_get_wtime();
+            auto startTime = std::chrono::high_resolution_clock::now();
+            #pragma omp parallel for default(none), firstprivate(startCell, rng), \
+            shared(solution_found, solution_thread_n, maze, std::cout, startTime, run, maze_sum_run_time)
+            for (int i = 0; i < N_PARTICLES; i++) {
+//                double endTime = 0;
+                auto endTime = startTime;
+                Cell cell = startCell;
+                std::vector<std::pair<int, int>> path;
+                bool out = false;
+                while (!out && !solution_found) {
+                    path.push_back(std::pair(cell.x, cell.y));
+                    int random = rng.uniform(0, cell.numPossibleDirections);
+                    try {
+                        cell = maze.move(cell, cell.getDirectionFromIndex(random));
+                    }
+                    catch (OutOfMazeException &e) {
+                        out = true;
+                        solution_found = true;
+//                        endTime = omp_get_wtime();
+                        endTime = std::chrono::high_resolution_clock::now();
+                        solution_thread_n = getThreadNumber();
+                    }
+                }
+                if (out && solution_thread_n == getThreadNumber())
+                {
+                    std::cout << "Thread " << getThreadNumber() << ", particle " << i;
+//                    std::cout << "\t " << endTime - startTime << "s" << std::endl;
+                    double s = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count() / 1000000000;
+                    std::cout << "\t " << s << "s" << std::endl;
+                    maze_sum_run_time += s;
+                    if (SAVE_SOLUTION_IMG && run == N_RUNS - 1) {
+                        maze.save_solution_image(path);
+                    }
+                }
             }
         }
-        if (out) {
-            #ifdef _OPENMP
-            std::cout << "Thread " << omp_get_thread_num() << ", particle " << i << std::endl;
-            #else
-            std::cout << "Particle " << i << std::endl;
-            #endif
-            maze.save_solution_image(path);
-        }
+        std::cout << "Maze " << maze_index << " average run time:" << maze_sum_run_time / N_RUNS << std::endl;
+        mazes_total_sum_run_time += maze_sum_run_time;
     }
+    std::cout << std::endl << "Mazes total average run time:" << mazes_total_sum_run_time / N_MAZES << std::endl;
     return 0;
-}
-
-std::string pathToString(std::vector<std::pair<int, int>> &path) {
-    std::string s;
-    for (auto p: path) {
-        s.append("(" + std::to_string(p.first) + ", " + std::to_string(p.second) + "), ");
-    }
-    return s;
 }
