cbba5e4199f18a119a0df4a288c83c3b3df5c8d7
nekobanana <pru98@hotmail.it>
2023-12-22 19:14:35 +0100

    Results

--- results_postproc/graphs.py
+++ results_postproc/graphs.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("results.csv", sep="; ")
 n_particles = 100
 for maze_name in df["maze"].unique():
-    maze_df = df.loc[(df["maze"] == maze_name) & (df["n_particles"] == n_particles)]
+    maze_df = df.loc[(df["maze"] == maze_name) & ((df["n_particles"] == n_particles) | df["n_threads"] == 1)]
     maze_size = maze_df["maze_size"].iloc[0]
     maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
                  title=f"Maze {maze_size}, {n_particles=}",
--- results_postproc/results.py
+++ results_postproc/results.py
@@ -4,23 +4,24 @@ import os
 output_filename = "results.csv"
 execution_times = {}
 
-for filename in os.listdir("../results"):
-    f = os.path.join(filename)
-    filename_match = re.search("(.+)_threads_(\d+)_p.txt", filename)
-    if os.path.isfile(f) and filename_match:
+results_dir = "../results"
+for filename in os.listdir(results_dir):
+    file = os.path.join(results_dir, filename)
+    filename_match = re.search("^(.+)_threads_(\d+)_p.txt$", filename)
+    if os.path.isfile(file) and filename_match:
         n_threads = filename_match.groups()[0]
         n_particles = filename_match.groups()[1]
-        with open(filename, "r") as f:
+        with open(file, "r") as f:
             lines = f.readlines()
         maze_idx = None
         maze_size = None
         for line in lines:
-            match = re.search("^Maze (\d+), (\d+x\d+)$", line)
+            match = re.search("^Maze \"?(.+)\"?, (\d+x\d+)$", line)
             if match:
                 maze_idx = match.groups()[0]
                 maze_size = match.groups()[1]
                 continue
-            match = re.search("^Maze (\d+) average run time:\s*(\d+\.?\d*)$", line)
+            match = re.search("^Maze \"?(.+)\"? average run time:\s*(\d+.?\d*)$", line)
             if match:
                 assert maze_idx == match.groups()[0]
                 maze_run_time = match.groups()[1]
