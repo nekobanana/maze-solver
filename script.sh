1fe0d2a8c83e326e54cba0ddfdd7d8315fdf6195
nekobanana <pru98@hotmail.it>
2023-12-22 18:58:15 +0100

    results

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./maze_solver_single_thread -p 1 -r 10
 ./maze_solver -p 100 -t 2 -r 10
 ./maze_solver -p 100 -t 4 -r 10
 ./maze_solver -p 100 -t 6 -r 10
@@ -10,3 +9,4 @@
 ./maze_solver -p 100 -t 16 -r 10
 ./maze_solver -p 100 -t 18 -r 10
 ./maze_solver -p 100 -t 20 -r 10
+./maze_solver_single_thread -p 1 -r 10
