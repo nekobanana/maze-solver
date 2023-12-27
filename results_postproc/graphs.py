a11a1f8d3bcfa76f16662372a710e62a213d4921
nekobanana <pru98@hotmail.it>
2023-12-27 15:03:32 +0100

    Results good

--- results_postproc/graphs.py
+++ results_postproc/graphs.py
@@ -2,28 +2,56 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.use("TkAgg")
+# mpl.use("TkAgg")
 
 
 def graphs_fixed_n_particles(df, n_particles):
     for maze_name in df["maze"].unique():
-        maze_df = df.loc[(df["maze"] == maze_name) & ((df["n_particles"] == n_particles) | (df["n_threads"] == 1))]
+        maze_df = (df.loc[(df["maze"] == maze_name) & ((df["n_particles"] == n_particles) | (df["n_threads"] == 1))]
+                   .sort_values("n_threads"))
         maze_size = maze_df["maze_size"].iloc[0]
         maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
                      title=f"Maze {maze_size}, {n_particles=}",
                      xticks=np.concatenate((np.array([1]), np.arange(0, 22, 2))))
-        plt.show()
+        plt.savefig(f"plots/maze_{maze_size}_{n_particles=}.png")
 
 def graphs_linear_n_particles(df):
     for maze_name in df["maze"].unique():
-        maze_df = df.loc[(df["maze"] == maze_name) & (df["n_particles"] == df["n_threads"])]
+        maze_df = (df.loc[(df["maze"] == maze_name) & (df["n_particles"] == df["n_threads"])]
+                   .sort_values("n_threads"))
         maze_size = maze_df["maze_size"].iloc[0]
         maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
                      title=f"Maze {maze_size}, n_particles=n_threads",
                      xticks=np.concatenate((np.array([1]), np.arange(0, 22, 2))))
-        plt.show()
+        plt.savefig(f"plots/maze_{maze_size}_n_particles=n_threads.png")
+
+
+def graphs_all_particles_and_speedup(df):
+    maze_avg = df.groupby(by=["maze", "maze_size", "n_threads"])["execution_time"].sum().reset_index()
+    for maze_name in maze_avg["maze"].unique():
+        maze_df = maze_avg.loc[(maze_avg["maze"] == maze_name)].sort_values("n_threads")
+        maze_size = maze_df["maze_size"].iloc[0]
+        plt.figure()
+        maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
+                     title=f"Maze {maze_size}",
+                     xticks=np.concatenate((np.array([1]), np.arange(0, 22, 2))))
+        plt.savefig(f"plots/maze_{maze_size}.png")
+        plt.close()
+
+        sequential_exec_time = (maze_df.loc[(maze_df["n_threads"] == 1)])["execution_time"].iloc[0]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        speedup = sequential_exec_time / maze_df["execution_time"]
+        plt.scatter(maze_df["n_threads"], speedup, color="orange")
+        plt.title(f"Speedup maze {maze_size}")
+        plt.xticks(np.concatenate((np.array([1]), np.arange(0, 22, 2))))
+        for t, s in zip(maze_df["n_threads"], speedup):
+            ax.annotate(f"  {s:.2}", xy=(t, s), xytext=(t, s), xycoords='data')
+        plt.savefig(f"plots/speedup_maze_{maze_size}.png")
+        plt.close()
 
 if __name__ == "__main__":
     df = pd.read_csv("results.csv", sep="; ")
-    graphs_fixed_n_particles(df, n_particles=100)
+    # graphs_fixed_n_particles(df, n_particles=100)
     # graphs_linear_n_particles(df)
+    graphs_all_particles_and_speedup(df)
--- script2.sh
+++ script2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-./maze_solver -p 2 -t 2 -r 10
+#./maze_solver -p 2 -t 2 -r 10
 ./maze_solver -p 4 -t 4 -r 10
-./maze_solver -p 6 -t 6 -r 10
+./maze_solver -p 100 -t 4 -r 10
+#./maze_solver -p 6 -t 6 -r 10
 ./maze_solver -p 8 -t 8 -r 10
 ./maze_solver -p 10 -t 10 -r 10
 ./maze_solver -p 12 -t 12 -r 10
@@ -9,4 +10,5 @@
 ./maze_solver -p 16 -t 16 -r 10
 ./maze_solver -p 18 -t 18 -r 10
 ./maze_solver -p 20 -t 20 -r 10
-./maze_solver_single_thread -p 1 -r 10
+./maze_solver -p 100 -t 20 -r 10
+#./maze_solver_single_thread -p 1 -r 10
