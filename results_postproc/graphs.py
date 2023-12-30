44657100efa1b020d8525ee64dea42d0f11efae0
nekobanana <pru98@hotmail.it>
2023-12-30 11:51:52 +0100

    Other graphs

--- results_postproc/graphs.py
+++ results_postproc/graphs.py
@@ -4,27 +4,27 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 # mpl.use("TkAgg")
 
-
-def graphs_fixed_n_particles(df, n_particles):
-    for maze_name in df["maze"].unique():
-        maze_df = (df.loc[(df["maze"] == maze_name) & ((df["n_particles"] == n_particles) | (df["n_threads"] == 1))]
-                   .sort_values("n_threads"))
-        maze_size = maze_df["maze_size"].iloc[0]
-        maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
-                     title=f"Maze {maze_size}, {n_particles=}",
-                     xticks=np.concatenate((np.array([1]), np.arange(0, 32, 2))))
-        plt.savefig(f"plots/maze_{maze_size}_{n_particles=}.png")
-
-def graphs_linear_n_particles(df):
-    for maze_name in df["maze"].unique():
-        maze_df = (df.loc[(df["maze"] == maze_name) & (df["n_particles"] == df["n_threads"])]
-                   .sort_values("n_threads"))
-        maze_size = maze_df["maze_size"].iloc[0]
-        maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
-                     title=f"Maze {maze_size}, n_particles=n_threads",
-                     xticks=np.concatenate((np.array([1]), np.arange(0, 32, 2))))
-        plt.savefig(f"plots/maze_{maze_size}_n_particles=n_threads.png")
-
+#
+# def graphs_fixed_n_particles(df, n_particles):
+#     for maze_name in df["maze"].unique():
+#         maze_df = (df.loc[(df["maze"] == maze_name) & ((df["n_particles"] == n_particles) | (df["n_threads"] == 1))]
+#                    .sort_values("n_threads"))
+#         maze_size = maze_df["maze_size"].iloc[0]
+#         maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
+#                      title=f"Maze {maze_size}, {n_particles=}",
+#                      xticks=np.concatenate((np.array([1]), np.arange(0, 32, 2))))
+#         plt.savefig(f"plots/maze_{maze_size}_{n_particles=}.png")
+#
+# def graphs_linear_n_particles(df):
+#     for maze_name in df["maze"].unique():
+#         maze_df = (df.loc[(df["maze"] == maze_name) & (df["n_particles"] == df["n_threads"])]
+#                    .sort_values("n_threads"))
+#         maze_size = maze_df["maze_size"].iloc[0]
+#         maze_df.plot(x="n_threads", y="execution_time", kind="scatter",
+#                      title=f"Maze {maze_size}, n_particles=n_threads",
+#                      xticks=np.concatenate((np.array([1]), np.arange(0, 32, 2))))
+#         plt.savefig(f"plots/maze_{maze_size}_n_particles=n_threads.png")
+#
 
 def graphs_all_particles_and_speedup(df):
     maze_avg = df.groupby(by=["maze", "maze_size", "n_threads"])["execution_time"].sum().reset_index()
@@ -44,10 +44,13 @@ def graphs_all_particles_and_speedup(df):
         speedup = sequential_exec_time / maze_df["execution_time"]
         plt.scatter(maze_df["n_threads"], speedup, color="orange")
         plt.title(f"Speedup maze {maze_size}")
+        plt.savefig(f"plots/speedup_maze_{maze_size}.png")
         plt.xticks(np.concatenate((np.array([1]), np.arange(0, 32, 2))))
         for t, s in zip(maze_df["n_threads"], speedup):
             ax.annotate(f"  {s:.2}", xy=(t, s), xytext=(t, s), xycoords='data')
-        plt.savefig(f"plots/speedup_maze_{maze_size}.png")
+            plt.plot([t, t], [0, s], linestyle='dashed', color="orange")
+        plt.gca().set_ylim(bottom=0)
+        plt.savefig(f"plots/speedup_maze_{maze_size}_annotated.png")
         plt.close()
 
 if __name__ == "__main__":
--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#./maze_solver_single_thread -p 100 -r 100
-#./maze_solver -p 100 -t 2 -r 100
-#./maze_solver -p 100 -t 4 -r 100
-#./maze_solver -p 100 -t 6 -r 100
-#./maze_solver -p 100 -t 8 -r 100
-#./maze_solver -p 100 -t 10 -r 100
-#./maze_solver -p 100 -t 12 -r 100
-#./maze_solver -p 100 -t 14 -r 100
+./maze_solver_single_thread -p 100 -r 100
+./maze_solver -p 100 -t 2 -r 100
+./maze_solver -p 100 -t 4 -r 100
+./maze_solver -p 100 -t 6 -r 100
+./maze_solver -p 100 -t 8 -r 100
+./maze_solver -p 100 -t 10 -r 100
+./maze_solver -p 100 -t 12 -r 100
+./maze_solver -p 100 -t 14 -r 100
 ./maze_solver -p 100 -t 16 -r 100
 ./maze_solver -p 100 -t 18 -r 100
 ./maze_solver -p 100 -t 20 -r 100
